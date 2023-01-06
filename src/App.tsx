73d4bc226511d95057c8ceb6c0f487e627f0b4e4
Kailey <k.kotynski@gmail.com>
2023-01-05 19:18:52 -0800

    Added personList to work with props that use an array

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import "./App.css";
 import { Greet } from "./components/Greet";
 import { Person } from "./components/Person";
+import { PersonList } from "./components/PersonList";
 
 function App() {
   const personName = {
     first: "Kailey",
     last: "Kotynski",
   };
+
+  const personList = [
+    {
+      first: "Kailey",
+      last: "Kotynski",
+    },
+    {
+      first: "Omkar",
+      last: "Rane",
+    },
+  ];
   return (
     <div className="App">
       <Greet name="Kailey" messageCount={10} isLoggedIn={false} />
       <Person name={personName} />
+      <PersonList names={personList} />
     </div>
   );
 }
